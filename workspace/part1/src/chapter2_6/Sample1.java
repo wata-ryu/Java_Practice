e4dfd8e7fd5cb735f1a387dbb0927dd3d8a14bb6
渡辺龍之介 <ryuunosuke@watanaberyuunosukenoMacBook-Pro.local>
2023-05-22 18:28:47 +0900

    演算子と演算:23/5/22

---
+++ workspace/part1/src/chapter2_6/Sample1.java
@@ -0,0 +1,13 @@
+package chapter2_6;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		int num1 = 10, num2=10;
+		++num1;	//インクリメント
+		System.out.println(num1);
+		--num2;	//デクリメント
+		System.out.println(num2);
+	}
+
+}
---
+++ workspace/part1/src/chapter2_6/Sample3.java
@@ -0,0 +1,14 @@
+package chapter2_6;
+
+public class Sample3 {
+
+	public static void main(String[] args) {
+		String msg;
+		msg = "答えは" + "100です"; //文字同士の連結
+		System .out.println(msg);
+		
+		msg = "答えは" + 100;	//文字以外とも連結できる
+		System.out.println(msg);
+	}
+
+}
