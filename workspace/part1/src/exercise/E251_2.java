a9dd7ec573fc0c0de14da88c6fc700ca6b255d2d
渡辺龍之介 <ryuunosuke@watanaberyuunosukenoMacBook-Pro.local>
2023-05-22 16:11:02 +0900

    練習2-5-1:23/5/22

---
+++ workspace/part1/src/exercise/E251_2.java
@@ -0,0 +1,11 @@
+package exercise;
+
+public class E251_2 {
+
+	public static void main(String[] args) {
+		System.out.println(1000 + "\t田中宏");
+		System.out.println(1010 + "\t川谷太郎");
+		System.out.println(1020 + "\t鈴木誠");
+	}
+
+}
---
+++ workspace/part1/src/exercise/E251_3.java
@@ -0,0 +1,14 @@
+package exercise;
+
+public class E251_3 {
+
+	public static void main(String[] args) {
+		String str = """
+				1000	田中宏
+				1010		谷川太郎
+				1020	鈴木誠
+				""";
+				System.out.println(str);
+	}
+
+}
