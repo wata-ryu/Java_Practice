b788593943c2cb961e05edfaf010c47a5295f0ba
wata-ryu <71304058ryu@gmail.com>
2023-06-07 23:43:58 +0900

    コンストラクタ3

--- workspace/part2/src/chapter7_2/Sample2.java
+++ workspace/part2/src/chapter7_2/Sample2.java
@@ -3,7 +3,7 @@ package chapter7_2;
 public class Sample2 {
 
 	public static void main(String[] args) {
-
+		
 	}
 
 }
--- workspace/part2/src/chapter7_3/Joining.java
+++ workspace/part2/src/chapter7_3/Joining.java
@@ -1,5 +1,17 @@
 package chapter7_3;
 
 public class Joining {
-
+	private String str;
+	private String delimiter;
+	
+	public Joining(String str, String delimiter) {
+		this.str = str;
+		this.delimiter = delimiter;
+	}
+	public Joining(String delimiter) {
+		this("", delimiter);
+	}
+	public Joining() {
+		this("", ",");
+	}
 }
