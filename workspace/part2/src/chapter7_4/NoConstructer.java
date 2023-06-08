0e2696c422dbe8060419fc4ad75ae95bcd2ff56d
wata-ryu <71304058ryu@gmail.com>
2023-06-08 19:31:56 +0900

    コンストラクタ4

---
+++ workspace/part2/src/chapter7_4/NoConstructer.java
@@ -0,0 +1,16 @@
+package chapter7_4;
+
+public class NoConstructer {
+	private int number;
+	private String str = "***";	//空文字で初期化
+	
+	//ゲッター
+	public int getNumber() {
+		return number;
+	}
+	public String getStr() {
+		return str;
+	}
+	
+
+}
